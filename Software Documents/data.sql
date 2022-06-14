7615458f592b64214bc5b1cd83893dc825de8010
DaanBrouwer <daanbrouwer96@gmail.com>
2022-06-14 14:17:30 +0200

    Upload Docx

---
+++ Software Documents/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO `USER` (`USERID`, `ADMIN`, `MAIL_ADDRESS`, `PASSWORD_HASH`, `PASSWORD_SALT`, `USER_NAME`)
+VALUES (1, 1, 'VanHeeschs@Test.nl', 12, 13, 'TestVanHees'),
+       (2, 1, 'Cone@Test.com', 11, 55, 'TestPylon');
+
+INSERT INTO `SENSOR_TYPE` (`TYPEID`, `TYPE_NAME`)
+VALUES (1, 'Temperatuur'),
+       (2, 'Stikstof'),
+       (3, 'Koolstofdioxide'),
+       (4, 'Fijnstof'),
+       (5, 'Luchtvochtigheid'),
+       (6, 'Windsnelheid');
+
+INSERT INTO `LOCATION` (`LOCATIONID`, `LATITUDE`, `LONGITUDE`, `LOCATION_NAME`)
+VALUES (1, 51.581124, 4.994231, 'Reeshof'),
+       (2, 51.575043, 5.002305, 'Reeshof'),
+       (3, 51.539151, 5.079001, 'Stappegoor');
+
+INSERT INTO `STATION` (`STATIONID`, `NAME`, `HEIGHT`, `LOCATION_LOCATIONID`, `USERID`, `IS_PUBLIC`)
+VALUES (1, 'Voortuin', 1.0, 1, 2, true),
+       (2, 'Industrieterrein', 6.0, 2, 1, false),
+       (3, 'Fontys dak', 38.2, 3, 1, true);
+
+INSERT INTO `SENSOR` (`SENSORID`, `SENSOR_DATA`, `SENSOR_TYPEID`, `STATIONID`)
+VALUES (1, 2, 1, 1),
+       (2, 3, 1, 2),
+       (3, 4, 1, 3),
+       (4, 1, 2, 1),
+       (5, 5, 2, 2),
+       (6, 3, 2, 3),
+       (7, 4, 3, 1),
+       (8, 6, 3, 2),
+       (9, 2, 3, 3),
+       (10, 4, 4, 1),
+       (11, 1, 4, 2),
+       (12, 1, 4, 3),
+       (13, 10, 5, 1),
+       (14, 1, 5, 2),
+       (15, 4, 5, 3),
+       (16, 6, 6, 1),
+       (17, 5, 6, 2),
+       (18, 3, 6, 3);
+
